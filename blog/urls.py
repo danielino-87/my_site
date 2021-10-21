3f177961839dddffe56d9142e5dcb219512345c1
danielino-87 <danieleiovinelli@gmail.com>
2021-10-21 14:42:05 +0200

    Create Gitignore File

---
+++ blog/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+   path("" , views.starting_page ,name="starting-page"), # /blog/
+   path("posts" , views.posts , name= "posts-page"),
+   path("posts/<slug:slug>" , views.single_post , name="post-detail-page") #/posts/my-first-post
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import Http404  , HttpResponse , HttpResponseNotFound ,HttpResponseRedirect, response
+from django.urls import reverse
+from django.template.loader import render_to_string
 
-# Create your views here.
+from django.shortcuts import render
+
+def starting_page (request):
+    pass
+
+def posts (request , post):
+    pass
+
+def single_post(request):
+    pass

96dda214994187ca3f21a9c111578f56bd22c4bc
danielino-87 <danieleiovinelli@gmail.com>
2021-10-22 11:56:04 +0200

    Create Gitignore File

--- blog/views.py
+++ blog/views.py
@@ -8,8 +8,8 @@ from django.shortcuts import render
 def starting_page (request):
     return render (request , "blog/index.html")
 
-def posts (request , post):
-    pass
+def posts (request):
+    return render (request,"blog/all-posts.html" )
 
 def single_post(request):
     pass

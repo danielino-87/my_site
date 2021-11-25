db6f7e206fb512fc1d1c064f6a185974fb46cf57
danielino-87 <danieleiovinelli@gmail.com>
2021-11-25 16:13:21 +0100

    Create Gitignore File

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.core.validators import EmailValidator, MinValueValidator , MaxValueValidator , MinLengthValidator
+from django.db.models.deletion import CASCADE
+from django.urls import reverse
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=20)
+    def __str__(self):
+        return f"{self.caption}"
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email_address = models.EmailField()
+    
+    def all_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self) :
+        return self.all_name()
+
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL ,null=True, related_name="posts")
+    exerpt = models.CharField(max_length=200)
+    image_name = models.CharField(max_length=100)
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(unique=True , default="",blank=True ,null=False, db_index=True) 
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    tags = models.ManyToManyField(Tag)
+
+
+    def get_absolute_url(self):
+        return reverse ("post-detail-page" , args=[self.slug])
+
+    def __str__(self):
+        return f"{self.title} ({self.exerpt})" 
+
+
+
+
+

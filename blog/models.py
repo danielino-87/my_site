ec303b8d2ec0022c7a7de0650217cf649a48eaef
danielino-87 <danieleiovinelli@gmail.com>
2021-12-31 12:23:44 +0100

    Create Gitignore File

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,14 @@
 from django.db import models
-from django.core.validators import EmailValidator, MinValueValidator , MaxValueValidator , MinLengthValidator
-from django.db.models.deletion import CASCADE
-from django.urls import reverse
-from django.utils.text import slugify
+from django.core.validators import MinLengthValidator
+
+# Create your models here.
 
 
 class Tag(models.Model):
     caption = models.CharField(max_length=20)
+
     def __str__(self):
-        return f"{self.caption}"
+        return self.caption
 
 
 class Author(models.Model):
@@ -16,36 +16,31 @@ class Author(models.Model):
     last_name = models.CharField(max_length=100)
     email_address = models.EmailField()
 
-    def all_name(self):
+    def full_name(self):
         return f"{self.first_name} {self.last_name}"
 
-    def __str__(self) :
-        return self.all_name()
-
-    
-
-
+    def __str__(self):
+        return self.full_name()
 
 
 class Post(models.Model):
     title = models.CharField(max_length=150)
-    author = models.ForeignKey(Author, on_delete=models.SET_NULL ,null=True, related_name="posts")
-    exerpt = models.CharField(max_length=200)
-    image_name = models.CharField(max_length=100)
+    excerpt = models.CharField(max_length=200)
+    image = models.ImageField(upload_to="posts", null=True)
     date = models.DateField(auto_now=True)
-    slug = models.SlugField(unique=True , default="",blank=True ,null=False, db_index=True) 
+    slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
+    author = models.ForeignKey(
+        Author, on_delete=models.SET_NULL, null=True, related_name="posts")
     tags = models.ManyToManyField(Tag)
 
-    def get_absolute_url(self):
-        return reverse ("post-detail-page" , args=[self.slug])
+    def __str__(self):
+        return self.title
 
 
-
-
-
-
-
-
-
-
+class Comment(models.Model):
+    user_name = models.CharField(max_length=120)
+    user_email = models.EmailField() 
+    text = models.TextField(max_length=400)
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
--- my_site/urls.py
+++ my_site/urls.py
@@ -1,4 +1,4 @@
-"""mysite URL Configuration
+"""my_site URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path , include
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("" , include("blog.urls")) # http//localhost:8000/
-]
+    path("", include("blog.urls"))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
